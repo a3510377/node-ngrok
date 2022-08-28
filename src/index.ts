1bc6632922a40e5de3a82804caad9e3b989ff7be
猴子 <a102009102009@gmail.com>
2022-08-28 10:37:07 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import ngrok, { Ngrok } from "ngrok";
+import { createServer } from "http";
 
 (async () => {
   const { HOST, PORT, AUTH_TOKEN, REGION, PROTO } = process.env;
@@ -7,7 +8,16 @@ import ngrok, { Ngrok } from "ngrok";
     addr: parseInt(PORT || "") || HOST,
     proto: <Ngrok.Protocol | undefined>PROTO,
     region: <Ngrok.Region | undefined>REGION,
+    onLogEvent: console.info,
   });
+
+  createServer((req, res) => {
+    if (req.method === "GET" && req.url === "/") {
+      res
+        .writeHead(200, { "Content-Type": "text/plain; charset=utf-8" })
+        .end(url);
+    }
+  }).listen(1333, () => console.log("listening on port 1333"));
 
   console.log(url);
 })();
